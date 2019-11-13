b3fb5ebe7446f6122fff33e53dd457723a42de65
Rafael Augusto <rjaugustobe@yahoo.com.br>
2019-11-13 14:07:00 -0300

    Criando operação de instanciação e inclusão de Skins no Banco de dados

--- src/main/java/com/rafael/skinator/SkinatorApplication.java
+++ src/main/java/com/rafael/skinator/SkinatorApplication.java
@@ -1,13 +1,40 @@
 package com.rafael.skinator;
 
+import static com.rafael.skinator.skin.domain.Exterior.BATTLE_SCARRED;
+import static com.rafael.skinator.skin.domain.Exterior.WELL_WORN;
+import static com.rafael.skinator.skin.domain.StatTrack.N;
+import static com.rafael.skinator.skin.domain.StatTrack.S;
+import static com.rafael.skinator.skin.domain.Type.ASSAUT_RIFFLE;
+import static com.rafael.skinator.skin.domain.Type.SNIPER_RIFLE;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.rafael.skinator.skin.domain.Skin;
+import com.rafael.skinator.skin.repositores.SkinRepository;
+
 @SpringBootApplication
-public class SkinatorApplication {
+public class SkinatorApplication implements CommandLineRunner {
+	
+	@Autowired
+	private SkinRepository skinRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkinatorApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Skin skin1 = new Skin(null, "Ak 47 grade de Safari", BATTLE_SCARRED, ASSAUT_RIFFLE, S, 10.60, 0.01);
+		Skin skin2 = new Skin(null, "AWP ATHERIS", WELL_WORN, SNIPER_RIFLE, N, 150.60, 0.02);
+		
+		skinRepository.saveAll(Arrays.asList(skin1,skin2));
+		
 	}
 
 }
